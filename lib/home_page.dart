450b91a3f54b48c9adbc72cda31c8a6f3cd354dd
DjeghlafLydia <djeghlaf.habiba@gmail.com>
2025-02-09 12:19:08 +0100

    main logic completed

---
+++ lib/home_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/todo_tile.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //list of todo tasks
+  List toDoList = [
+    ["Learning flutter", false],
+    ["Make a todo App", false],
+    ["Make a Ecommerce App UI", false],
+  ];
+
+  //checkbox was tapped
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 246, 229, 252),
+      appBar: AppBar(
+        title: Text(
+          "My Todo List",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return TodoTile(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) => checkBoxChanged(value, index),
+          );
+        },
+      ),
+    );
+  }
+}
