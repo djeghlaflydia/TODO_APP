ab54cd30d88fd403d269f116c270251ab17c24c7
DjeghlafLydia <djeghlaf.habiba@gmail.com>
2025-02-12 12:16:23 +0100

    add logic

--- lib/dialog_box.dart
+++ lib/dialog_box.dart
@@ -1,20 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/buttons.dart';
 
 class DialogBox extends StatelessWidget {
-  const DialogBox({super.key});
+  final TextEditingController controller;
+  final VoidCallback onSave;
+  final VoidCallback onCancel;
+
+  DialogBox({
+    super.key,
+    required this.controller,
+    required this.onSave,
+    required this.onCancel,
+  });
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      backgroundColor: Colors.deepPurple,
-      content: Container(
+      backgroundColor: const Color.fromARGB(187, 104, 58, 183),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+        side: BorderSide(
+          color: const Color.fromARGB(255, 104, 58, 183),
+          width: 3,
+        ),
+      ),
+      content: SizedBox(
         height: 120,
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            //get user input
-            TextField(),
-
-            //buttons ->save + cancel
+            TextField(
+              controller: controller,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Add a new task",
+                hintStyle: TextStyle(color: Colors.white),
+              ),
+              style: TextStyle(color: Colors.white),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Buttons(text: "Save", onPressed: onSave),
+                const SizedBox(width: 8),
+                Buttons(text: "Cancel", onPressed: onCancel),
+              ],
+            ),
           ],
         ),
       ),
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,6 +10,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //text controller
+  final _controller = TextEditingController();
+
   //list of todo tasks
   List toDoList = [
     ["Learning flutter", false],
@@ -24,12 +27,25 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  //save nre task
+  void saveNewTask() {
+    setState(() {
+      toDoList.add([_controller.text, false]);
+      _controller.clear();
+    });
+    Navigator.of(context).pop();
+  }
+
   //create a new task
   void createNewTask() {
     showDialog(
       context: context,
       builder: (context) {
-        return DialogBox();
+        return DialogBox(
+          controller: _controller,
+          onSave: saveNewTask,
+          onCancel: () => Navigator.of(context).pop(),
+        );
       },
     );
   }
