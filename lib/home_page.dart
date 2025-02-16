ef1a2c2f2ea3cbd0eaa875bc64df4645f45d2aa4
DjeghlafLydia <djeghlaf.habiba@gmail.com>
2025-02-16 17:18:49 +0100

    logic done

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -50,6 +50,13 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+//delete task
+  void deleteTask(int index) {
+    setState(() {
+      toDoList.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -73,6 +80,7 @@ class _HomePageState extends State<HomePage> {
             taskName: toDoList[index][0],
             taskCompleted: toDoList[index][1],
             onChanged: (value) => checkBoxChanged(value, index),
+            deleteFunction: (context) => deleteTask(index),
           );
         },
       ),
--- lib/todo_tile.dart
+++ lib/todo_tile.dart
@@ -1,38 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class TodoTile extends StatelessWidget {
   final String taskName;
   final bool taskCompleted;
-  Function(bool?)? onChanged;
+  final Function(bool?)? onChanged;
+  final Function(BuildContext)? deleteFunction;
 
-  TodoTile(
-      {super.key,
-      required this.taskName,
-      required this.taskCompleted,
-      required this.onChanged});
+  TodoTile({
+    super.key,
+    required this.taskName,
+    required this.taskCompleted,
+    required this.onChanged,
+    required this.deleteFunction,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 25.0, right: 25.0, top: 25.0),
-      child: Container(
-        padding: EdgeInsets.all(16),
-        decoration: BoxDecoration(
-            color: const Color.fromARGB(80, 190, 171, 227),
-            borderRadius: BorderRadius.circular(12)
-        ),
-        child: Row(
+      child: Slidable(
+        key: ValueKey(taskName), // Required for auto-close behavior
+        endActionPane: ActionPane(
+          motion: const StretchMotion(),
           children: [
-            //checkbox
-            Checkbox(value: taskCompleted, onChanged: onChanged),
-
-            //task name
-            Text(taskName,
-                style: TextStyle(
-                    decoration: taskCompleted
-                        ? TextDecoration.lineThrough
-                        : TextDecoration.none)),
+            SlidableAction(
+              onPressed: deleteFunction, // Updated function type
+              icon: Icons.delete,
+              backgroundColor: const Color.fromARGB(161, 244, 67, 54),
+              borderRadius: BorderRadius.circular(12),
+              label: "Delete", // Required for newer versions
+            ),
           ],
+        ),
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(80, 190, 171, 227),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Row(
+            children: [
+              // Checkbox
+              Checkbox(value: taskCompleted, onChanged: onChanged),
+
+              // Task name
+              Text(
+                taskName,
+                style: TextStyle(
+                  decoration: taskCompleted
+                      ? TextDecoration.lineThrough
+                      : TextDecoration.none,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
